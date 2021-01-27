86261d65f631cea11ab449ea50b94b2bdb82f99b
Ashley Ruglys <ashley.ruglys@gmail.com>
2021-01-27 14:03:21 +0100

    cargo fmt

--- src/executor.rs
+++ src/executor.rs
@@ -33,8 +33,8 @@ use parity_scale_codec::DecodeAll as _;
 
 mod allocator; // TODO: make public after refactoring
 pub mod host;
-pub mod runtime_host;
 pub mod read_only_runtime_host;
+pub mod runtime_host;
 pub mod vm;
 
 /// Default number of heap pages if the storage doesn't specify otherwise.
