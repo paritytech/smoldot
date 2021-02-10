24ec7f42894584c508ddf926c4e7f07c01b922be
Raoul Millais <raoul@raoulmillais.com>
2021-02-10 11:14:27 +0000

    Change typings file license header to smoldot

--- bin/wasm-node/javascript/index.d.ts
+++ bin/wasm-node/javascript/index.d.ts
@@ -1,7 +1,20 @@
-// Copyright 2018-2021 @paritytech/substrate-light-ui authors & contributors
-// This software may be modified and distributed under the terms
-// of the Apache-2.0 license. See the LICENSE file for details.
+// Smoldot
+// Copyright (C) 2019-2021  Parity Technologies (UK) Ltd.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 declare class SmoldotError extends Error {
   constructor(message: string) {}
 }
