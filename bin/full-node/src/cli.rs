347b476cdf2e5b58707ce37a19202c4f84045027
Pierre Krieger <pierre.krieger1708@gmail.com>
2021-02-26 08:57:34 +0100

    Replace ed25519-dalek with ed25519-zebra

--- bin/full-node/src/cli.rs
+++ bin/full-node/src/cli.rs
@@ -173,7 +173,7 @@ impl core::str::FromStr for NodeKey {
         let mut out = [0; 32];
         out.copy_from_slice(&bytes);
 
-        ed25519_dalek::SecretKey::from_bytes(&out).map_err(|_| NodeKeyParseError::BadKey)?;
+        ed25519_zebra::SecretKey::from_bytes(&out).map_err(|_| NodeKeyParseError::BadKey)?;
 
         Ok(NodeKey(out))
     }
