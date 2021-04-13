73c973a6223354d6ffde7cc7f4b2f502e8f0babb
Pierre Krieger <pierre.krieger1708@gmail.com>
2021-04-13 09:52:10 +0200

    Move some nom combinators to util

--- src/network/protocol/storage_proof.rs
+++ src/network/protocol/storage_proof.rs
@@ -78,7 +78,7 @@ pub fn decode_storage_proof_response(
                 num_elems,
                 num_elems,
                 nom::combinator::map(
-                    nom::multi::length_data(crate::util::nom_scale_compact_usize),
+                    crate::util::nom_bytes_decode,
                     |b| b.to_vec(),
                 ),
             )
