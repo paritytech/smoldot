4ccf120b76719154ed249e187d132a1a6051eac9
Pierre Krieger <pierre.krieger1708@gmail.com>
2021-08-10 10:40:07 +0200

    Connect to a parachain in the demo (#1111)
    * Connect to a parachain in the demo
    
    * Print warning if JSON-RPC request takes a long time (#1112)
    
    * Add a liveness probe between index.js and worker.js (#1113)
    
    * Fix infinite loop
    
    * Fix demo for relay chains
    
    * Restore log level
    
    * Fix stupid bug in index.js
    
    * Only start the relay chain at initialization
    
    * Comments and tweaks to the demo

--- bin/wasm-node/javascript/demo/demo.js
+++ bin/wasm-node/javascript/demo/demo.js
@@ -21,10 +21,11 @@ import * as smoldot from '../src/index.js';
 import { default as websocket } from 'websocket';
 import * as http from 'http';
 import * as process from 'process';
-// Adjust these chain specs for the chain you want to connect to.
 import * as fs from 'fs';
 
+// Adjust these chain specs for the chain you want to connect to.
 const chainSpec = fs.readFileSync('../../westend.json', 'utf8');
+const parachainSpec = fs.readFileSync('../../westend-westmint.json', 'utf8');
 
 const client = smoldot.start({
     maxLogLevel: 3,  // Can be increased for more verbosity
@@ -33,72 +34,107 @@ const client = smoldot.start({
     forbidWss: false,
 });
 
-// Pre-load smoldot with the chain spec.
+// Pre-load smoldot with the relay chain spec.
 // We call `addChain` again with the same chain spec again every time a new WebSocket connection
 // is established, but smoldot will de-duplicate them and only connect to the chain once.
 // By calling it now, we let smoldot start syncing that chain in the background even before a
 // WebSocket connection has been established.
-const healthChecker = smoldot.healthChecker();
 client
-    .then(client => client.addChain({
-        chainSpec,
-        jsonRpcCallback: (response) => healthChecker.responsePassThrough(response)
-    }))
-    .then(chain => {
-        healthChecker.start((request) => chain.sendJsonRpc(request), (health) => console.log(health));
-    })
+    .then(client => client.addChain({ chainSpec }))
     .catch((error) => {
         console.error("Error while adding chain: " + error);
         process.exit(1);
     });
 
+// Start the WebSocket server listening on port 9944.
 let server = http.createServer(function (request, response) {
     response.writeHead(404);
     response.end();
 });
-
 server.listen(9944, function () {
     console.log('Server is listening on port 9944');
-    console.log('Visit https://polkadot.js.org/apps/?rpc=ws%3A%2F%2F127.0.0.1%3A9944');
+    console.log('Visit https://polkadot.js.org/apps/?rpc=ws%3A%2F%2F127.0.0.1%3A9944%2Frelay (relay chain) or https://polkadot.js.org/apps/?rpc=ws%3A%2F%2F127.0.0.1%3A9944%2Fparachain (parachain)');
 });
-
 let wsServer = new websocket.server({
     httpServer: server,
     autoAcceptConnections: false,
 });
 
 wsServer.on('request', function (request) {
+    // Received a new incoming WebSocket connection.
+
+    // Depending on the URL, we add either Westend or Westend+Westmint to smoldot.
+    // `chain` will contain a `Promise` that yields either `{ relay }` or `{ relay, para }`, where
+    // `relay` and `para` are of type `SmoldotChain`.
+    let chain;
+    if (request.resource == '/relay') {
+        chain = client.then(async client => {
+            return {
+                relay: await client.addChain({
+                    chainSpec,
+                    jsonRpcCallback: (resp) => {
+                        connection.sendUTF(resp);
+                    },
+                })
+            };
+        });
+
+    } else if (request.resource == '/parachain') {
+        chain = client.then(async client => {
+            const relay = await client.addChain({
+                chainSpec,
+            });
+
+            const para = await client.addChain({
+                chainSpec: parachainSpec,
+                jsonRpcCallback: (resp) => {
+                    connection.sendUTF(resp);
+                },
+                potentialRelayChains: [relay]
+            });
+
+            return { relay, para };
+        });
+    } else {
+        request.reject(404);
+        return;
+    }
+
     const connection = request.accept(request.requestedProtocols[0], request.origin);
     console.log((new Date()) + ' Connection accepted.');
 
-    const chain = client.then(client => {
-        return client.addChain({
-            chainSpec,
-            jsonRpcCallback: (resp) => {
-                connection.sendUTF(resp);
-            },
-        })
-            .catch((error) => {
-                console.error("Error while adding chain: " + error);
-                process.exit(1);
-            })
-    });
+    chain
+        .catch((error) => {
+            console.error("Error while adding chain: " + error);
+            connection.close(400);
+        });
 
+    // Receiving a message from the connection. This is a JSON-RPC request.
     connection.on('message', function (message) {
         if (message.type === 'utf8') {
             chain
-                .then(chain => chain.sendJsonRpc(message.utf8Data))
+                .then(chain => {
+                    if (chain.para)
+                        chain.para.sendJsonRpc(message.utf8Data);
+                    else
+                        chain.relay.sendJsonRpc(message.utf8Data);
+                })
                 .catch((error) => {
                     console.error("Error during JSON-RPC request: " + error);
                     process.exit(1);
                 });
         } else {
-            throw "Unsupported type: " + message.type;
+            connection.close(400);
         }
     });
 
+    // When the connection closes, remove the chains that have been added.
     connection.on('close', function (reasonCode, description) {
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-        chain.then(chain => chain.remove()).catch(() => { });
+        chain.then(chain => {
+            chain.relay.remove();
+            if (chain.para)
+                chain.para.remove();
+        }).catch(() => { });
     });
 });
