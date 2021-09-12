fe6defa4b08555017b103201e03d9ec306318aa0
Pierre Krieger <pierre.krieger1708@gmail.com>
2021-09-12 10:58:16 +0200

    all::blocks_request_response now returns the TRq

--- src/sync/all_forks.rs
+++ src/sync/all_forks.rs
@@ -471,7 +471,7 @@ impl<TBl, TRq, TSrc> AllForksSync<TBl, TRq, TSrc> {
             impl Iterator<Item = RequestSuccessBlock<impl AsRef<[u8]>, impl AsRef<[u8]>>>,
             (),
         >,
-    ) -> AncestrySearchResponseOutcome {
+    ) -> (TRq, AncestrySearchResponseOutcome) {
         // Sets the `occupation` of `source_id` back to `AllSync`.
         let (
             pending_blocks::RequestParams {
@@ -480,7 +480,7 @@ impl<TBl, TRq, TSrc> AllForksSync<TBl, TRq, TSrc> {
                 ..
             },
             source_id,
-            _, // TODO: unused
+            request_user_data,
         ) = self.inner.blocks.finish_request(request_id);
 
         // The body of this function mostly consists in verifying that the received answer is
@@ -534,7 +534,7 @@ impl<TBl, TRq, TSrc> AllForksSync<TBl, TRq, TSrc> {
                 false,
             ) {
                 HeaderFromSourceOutcome::HeaderVerify => {
-                    return AncestrySearchResponseOutcome::Verify;
+                    return (request_user_data, AncestrySearchResponseOutcome::Verify);
                 }
                 HeaderFromSourceOutcome::TooOld { .. } => {
                     // Block is below the finalized block number.
@@ -551,16 +551,18 @@ impl<TBl, TRq, TSrc> AllForksSync<TBl, TRq, TSrc> {
                     // on the finalized chain. It is possible that the finalized block has been
                     // updated between the moment the request was emitted and the moment the
                     // response is received.
-                    return AncestrySearchResponseOutcome::NotFinalizedChain {
+                    let outcome = AncestrySearchResponseOutcome::NotFinalizedChain {
                         discarded_unverified_block_headers: Vec::new(), // TODO:
                     };
+                    return (request_user_data, outcome);
                 }
                 HeaderFromSourceOutcome::AlreadyInChain => {
                     // Block is already in chain. Can happen if a different response or
                     // announcement has arrived and been processed between the moment the request
                     // was emitted and the moment the response is received.
                     debug_assert_eq!(index_in_response, 0);
-                    return AncestrySearchResponseOutcome::AllAlreadyInChain;
+                    let outcome = AncestrySearchResponseOutcome::AllAlreadyInChain;
+                    return (request_user_data, outcome);
                 }
                 HeaderFromSourceOutcome::Disjoint => {
                     // Block of unknown ancestry. Continue looping.
@@ -585,7 +587,10 @@ impl<TBl, TRq, TSrc> AllForksSync<TBl, TRq, TSrc> {
                 .remove_known_block(requested_block_height, requested_block_hash);
         }*/
 
-        AncestrySearchResponseOutcome::Inconclusive
+        (
+            request_user_data,
+            AncestrySearchResponseOutcome::Inconclusive,
+        )
     }
 
     /// Update the source with a newly-announced block.
