3891c289b8ed8f4bc8ec7234a7ab49993e0aa644
Pierre Krieger <pierre.krieger1708@gmail.com>
2021-11-29 09:08:55 +0100

    Fix prepare.js

--- bin/wasm-node/javascript/prepare.js
+++ bin/wasm-node/javascript/prepare.js
@@ -74,7 +74,7 @@ try {
     if (build_profile == 'release') {
         child_process.execSync(
             "wasm-opt -o src/autogen/tmp.wasm -Os --strip-debug --vacuum --dce "
-            + "../../../target/wasm32-wasi/" + build_profile + "/smoldot_light.wasm",
+            + "../../../target/wasm32-wasi/" + build_profile + "/smoldot_light_wasm.wasm",
             { 'stdio': 'inherit' }
         );
     } else {
@@ -86,7 +86,7 @@ try {
     fallback_copy = true;
 }
 if (fallback_copy) {
-    fs.copyFileSync("../../../target/wasm32-wasi/" + build_profile + "/smoldot_light.wasm", "./src/autogen/tmp.wasm");
+    fs.copyFileSync("../../../target/wasm32-wasi/" + build_profile + "/smoldot_light_wasm.wasm", "./src/autogen/tmp.wasm");
 }
 
 // We then base64-encode the `.wasm` file, and put this base64 string as a constant in
