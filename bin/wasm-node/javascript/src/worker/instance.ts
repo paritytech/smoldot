e9d2980b289468e082a3dd0569131a1102f3e41a
Pierre Krieger <pierre.krieger1708@gmail.com>
2022-01-11 19:58:09 +0100

    Fix missing import

--- bin/wasm-node/javascript/src/worker/instance.ts
+++ bin/wasm-node/javascript/src/worker/instance.ts
@@ -15,7 +15,9 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import * as pako from 'pako';
+import pako from 'pako';
+
+import { Buffer } from 'buffer';
 
 import { Config as SmoldotBindingsConfig, default as smoldot_light_builder } from './bindings-smoldot-light.js';
 import { Config as WasiConfig, default as wasi_builder } from './bindings-wasi.js';
