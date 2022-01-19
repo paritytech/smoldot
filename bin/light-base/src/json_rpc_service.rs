e187d30008fb1401c93769514a137606b3fcf8e0
Pierre Krieger <pierre.krieger1708@gmail.com>
2022-01-19 13:37:11 +0100

    Extract chain_unsubscribeAllHeads

--- bin/light-base/src/json_rpc_service.rs
+++ bin/light-base/src/json_rpc_service.rs
@@ -694,35 +694,7 @@ impl<TPlat: Platform> Background<TPlat> {
                     .await;
             }
             methods::MethodCall::chain_unsubscribeAllHeads { subscription } => {
-                let state_machine_subscription =
-                    if let Some((abort_handle, state_machine_subscription)) = self
-                        .subscriptions
-                        .lock()
-                        .await
-                        .misc
-                        .remove(&(subscription.to_owned(), SubscriptionTy::AllHeads))
-                    {
-                        abort_handle.abort();
-                        Some(state_machine_subscription)
-                    } else {
-                        None
-                    };
-
-                if let Some(state_machine_subscription) = &state_machine_subscription {
-                    self.requests_subscriptions
-                        .stop_subscription(state_machine_subscription)
-                        .await;
-                }
-
-                self.requests_subscriptions
-                    .respond(
-                        &state_machine_request_id,
-                        methods::Response::chain_unsubscribeAllHeads(
-                            state_machine_subscription.is_some(),
-                        )
-                        .to_json_response(request_id),
-                    )
-                    .await;
+                self.chain_unsubscribe_all_heads(request_id, &state_machine_request_id, subscription).await;
             }
             methods::MethodCall::chain_unsubscribeFinalizedHeads { subscription } => {
                 self.chain_unsubscribe_finalized_heads(request_id, &state_machine_request_id, subscription).await;
@@ -1278,6 +1250,41 @@ impl<TPlat: Platform> Background<TPlat> {
                     .await;
             }
         }
+    }
+
+    /// Handles a call to [`methods::MethodCall::chain_unsubscribeAllHeads`].
+    async fn chain_unsubscribe_all_heads(
+        self: &Arc<Self>,
+        request_id: &str,
+        state_machine_request_id: &requests_subscriptions::RequestId,
+        subscription: String,
+    ) {
+        let state_machine_subscription = if let Some((abort_handle, state_machine_subscription)) =
+            self.subscriptions
+                .lock()
+                .await
+                .misc
+                .remove(&(subscription.to_owned(), SubscriptionTy::AllHeads))
+        {
+            abort_handle.abort();
+            Some(state_machine_subscription)
+        } else {
+            None
+        };
+
+        if let Some(state_machine_subscription) = &state_machine_subscription {
+            self.requests_subscriptions
+                .stop_subscription(state_machine_subscription)
+                .await;
+        }
+
+        self.requests_subscriptions
+            .respond(
+                state_machine_request_id,
+                methods::Response::chain_unsubscribeAllHeads(state_machine_subscription.is_some())
+                    .to_json_response(request_id),
+            )
+            .await;
     }
 
     /// Handles a call to [`methods::MethodCall::chain_unsubscribeFinalizedHeads`].
