d7269b3c101f582cdd46f5469f2d8066c2134d04
Pierre Krieger <pierre.krieger1708@gmail.com>
2022-02-18 14:21:15 +0100

    Allow calling set_validation_result with the finalized block hash

--- src/transactions/light_pool/tests.rs
+++ src/transactions/light_pool/tests.rs
@@ -193,7 +193,7 @@ fn longevity_works_non_finalized() {
 }
 
 #[test]
-fn longevity_works_finalized_base() {
+fn longevity_works_finalized() {
     let mut pool = LightPool::new(Config {
         blocks_capacity: 16,
         finalized_block_hash: [0; 32],
@@ -245,6 +245,59 @@ fn longevity_works_finalized_base() {
     // One more block.
     pool.add_block([4; 32], &[3; 32], ());
     let _ = pool.set_best_block(&[4; 32]);
+
+    // Transaction is no longer valid because its longevity has expired.
+    assert!(!pool.is_valid_against_best_block(tx_id));
+    assert_eq!(
+        pool.unvalidated_transactions()
+            .map(|(id, _)| id)
+            .collect::<Vec<_>>(),
+        vec![tx_id]
+    );
+}
+
+#[test]
+fn longevity_works_finalized_base() {
+    let mut pool = LightPool::new(Config {
+        blocks_capacity: 16,
+        finalized_block_hash: [0; 32],
+        transactions_capacity: 16,
+    });
+
+    let tx_id = pool.add_unvalidated(vec![0], ());
+
+    // Validate transaction against that block.
+    pool.set_validation_result(
+        tx_id,
+        &[0; 32],
+        Ok(validate::ValidTransaction {
+            longevity: NonZeroU64::new(2).unwrap(),
+            priority: 1,
+            propagate: true,
+            provides: Vec::new(),
+            requires: Vec::new(),
+        }),
+    );
+    assert!(pool.is_valid_against_best_block(tx_id));
+    assert_eq!(pool.unvalidated_transactions().count(), 0);
+
+    // Add more blocks on top of the best chain.
+    pool.add_block([1; 32], &[0; 32], ());
+    let _ = pool.set_best_block(&[1; 32]);
+    pool.add_block([2; 32], &[1; 32], ());
+    let _ = pool.set_best_block(&[2; 32]);
+
+    // Finalize the latest block added.
+    let _ = pool.set_finalized_block(&[2; 32]);
+    let _ = pool.prune_finalized_with_body();
+
+    // The transaction is still valid.
+    assert!(pool.is_valid_against_best_block(tx_id));
+    assert_eq!(pool.unvalidated_transactions().count(), 0);
+
+    // One more block.
+    pool.add_block([3; 32], &[2; 32], ());
+    let _ = pool.set_best_block(&[3; 32]);
 
     // Transaction is no longer valid because its longevity has expired.
     assert!(!pool.is_valid_against_best_block(tx_id));
