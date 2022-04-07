a49039dd61a4800a131e22f82f3c9fbbf8daf007
Pierre Krieger <pierre.krieger1708@gmail.com>
2022-04-07 14:34:05 +0200

    Backport change to checkpoints format (#2219)
    * Backport change to checkpoints format
    
    * CHANGELOG PR
    
    * Fix warning
    
    * Rustfmt

--- src/chain_spec/light_sync_state.rs
+++ src/chain_spec/light_sync_state.rs
@@ -19,7 +19,7 @@ use super::{ParseError, ParseErrorInner};
 use crate::header::BabeNextConfig;
 
 use alloc::{collections::BTreeMap, format, string::String, vec::Vec};
-use parity_scale_codec::{Decode, DecodeAll as _, Encode};
+use parity_scale_codec::{Decode, Encode};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -41,10 +41,13 @@ impl LightSyncState {
             finalized_block_header: crate::header::decode(&self.finalized_block_header.0[..])
                 .map_err(|_| ParseError(ParseErrorInner::Other))?
                 .into(),
-            // Note that for some reason `decode_all` accepts a `&mut &[u8]` and makes it empty.
-            grandpa_authority_set: AuthoritySet::decode_all(&mut grandpa_authority_set_slice)
+            // We use `decode` in order to remain compatible in case new fields are added in
+            // Substrate to these data structures.
+            // This really should be solved by having a proper format for checkpoints, but
+            // there isn't.
+            grandpa_authority_set: AuthoritySet::decode(&mut grandpa_authority_set_slice)
                 .map_err(|_| ParseError(ParseErrorInner::Other))?,
-            babe_epoch_changes: EpochChanges::decode_all(&mut babe_epoch_changes_slice)
+            babe_epoch_changes: EpochChanges::decode(&mut babe_epoch_changes_slice)
                 .map_err(|_| ParseError(ParseErrorInner::Other))?,
         };
 
@@ -63,6 +66,17 @@ pub(super) struct DecodedLightSyncState {
 pub(super) struct EpochChanges {
     inner: ForkTree<PersistedEpochHeader>,
     pub(super) epochs: BTreeMap<([u8; 32], u32), PersistedEpoch>,
+    // TODO: Substrate has added the field below to the checkpoints format ; it is commented out
+    //       right now in order to maintain compatibility with checkpoints that were generated
+    //       a long time ago
+    // gap: Option<GapEpochs>,
+}
+
+#[allow(unused)]
+#[derive(Debug, Decode, Encode)]
+pub(super) struct GapEpochs {
+    current: ([u8; 32], u32, PersistedEpoch),
+    next: Option<([u8; 32], u32, BabeEpoch)>,
 }
 
 #[derive(Debug, Decode, Encode)]
