6adec9885fc95428452b50c721f4d536658a0b5f
Pierre Krieger <pierre.krieger1708@gmail.com>
2022-05-24 12:21:15 +0200

    Remove protocol_index field when no longer necessary

--- src/libp2p/connection/established/substream.rs
+++ src/libp2p/connection/established/substream.rs
@@ -852,7 +852,7 @@ where
                 } else {
                     (
                         Some(SubstreamInner::NotificationsInRefused),
-                        Some(Event::NotificationsInOpenCancel { protocol_index }),
+                        Some(Event::NotificationsInOpenCancel),
                     )
                 }
             }
@@ -885,7 +885,6 @@ where
                         return (
                             Some(SubstreamInner::NotificationsInClosed),
                             Some(Event::NotificationsInClose {
-                                protocol_index,
                                 outcome: Ok(()),
                             }),
                         );
@@ -929,7 +928,6 @@ where
                     Err(error) => (
                         Some(SubstreamInner::NotificationsInClosed),
                         Some(Event::NotificationsInClose {
-                            protocol_index,
                             outcome: Err(NotificationsInClosedErr::ProtocolError(error)),
                         }),
                     ),
@@ -1119,12 +1117,11 @@ where
                 response: Err(RequestError::SubstreamReset),
             }),
             SubstreamInner::NotificationsInHandshake { .. } => None,
-            SubstreamInner::NotificationsInWait { protocol_index, .. } => {
-                Some(Event::NotificationsInOpenCancel { protocol_index })
+            SubstreamInner::NotificationsInWait { .. } => {
+                Some(Event::NotificationsInOpenCancel)
             }
-            SubstreamInner::NotificationsIn { protocol_index, .. } => {
+            SubstreamInner::NotificationsIn { .. } => {
                 Some(Event::NotificationsInClose {
-                    protocol_index,
                     outcome: Err(NotificationsInClosedErr::SubstreamReset),
                 })
             }
@@ -1482,10 +1479,7 @@ pub enum Event<TRqUd, TNotifUd> {
     /// This can only happen after [`Event::NotificationsInOpen`].
     /// [`Substream::accept_in_notifications_substream`] or
     /// [`Substream::reject_in_notifications_substream`] should not be called on this substream.
-    NotificationsInOpenCancel {
-        /// Index of the notifications protocol concerned by the substream.
-        protocol_index: usize,
-    },
+    NotificationsInOpenCancel,
     /// Remote has sent a notification on an inbound notifications substream. Can only happen
     /// after the substream has been accepted.
     // TODO: give a way to back-pressure notifications
@@ -1500,8 +1494,6 @@ pub enum Event<TRqUd, TNotifUd> {
     NotificationsInClose {
         /// If `Ok`, the substream has been closed gracefully. If `Err`, a problem happened.
         outcome: Result<(), NotificationsInClosedErr>,
-        /// Index of the notifications protocol concerned by the substream.
-        protocol_index: usize,
     },
 
     /// Remote has accepted or refused a substream opened with [`Substream::notifications_out`].
