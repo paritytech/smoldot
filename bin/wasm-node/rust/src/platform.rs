9c0d414df36932233d8f7f59c061a761879c9fc8
Pierre Krieger <pierre.krieger1708@gmail.com>
2022-06-24 17:11:49 +0200

    Spellcheck

--- bin/wasm-node/rust/src/platform.rs
+++ bin/wasm-node/rust/src/platform.rs
@@ -459,8 +459,8 @@ enum ConnectionInner {
     MultiStream {
         /// Peer id we're connected to.
         peer_id: smoldot_light_base::PeerId,
-        /// List of substreams that the host (i.e. JS side) has reported have been opened, but
-        /// that haven't been reported through [`Platform::next_substream`] yet.
+        /// List of substreams that the host (i.e. JavaScript side) has reported have been opened,
+        /// but that haven't been reported through [`Platform::next_substream`] yet.
         opened_substreams_to_pick_up: VecDeque<(u32, PlatformSubstreamDirection)>,
         /// Number of objects (connections and streams) in the [`Platform`] API that reference
         /// this connection. If it switches from 1 to 0, the connection must be removed.
