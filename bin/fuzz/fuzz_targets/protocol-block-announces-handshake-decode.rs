5f95d57b592f783c8cb5fbcd4e39aa76196c3956
Pierre Krieger <pierre.krieger1708@gmail.com>
2022-07-08 13:23:08 +0200

    Oops, fix fuzzing targets again

--- bin/fuzz/fuzz_targets/protocol-block-announces-handshake-decode.rs
+++ bin/fuzz/fuzz_targets/protocol-block-announces-handshake-decode.rs
@@ -19,7 +19,7 @@
 
 libfuzzer_sys::fuzz_target!(|params: (u8, &[u8])| {
     let _ = smoldot::network::protocol::decode_block_announces_handshake(
-        usize::from(params.0),
-        params.1 + 1,
+        usize::from(params.0) + 1,
+        params.1,
     );
 });
