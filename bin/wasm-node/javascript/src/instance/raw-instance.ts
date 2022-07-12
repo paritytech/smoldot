b31d4630f643d663c61ab0273e45bcad8bc8d0df
Pierre Krieger <pierre.krieger1708@gmail.com>
2022-07-12 13:28:44 +0200

    Pass onProcExit rather than throwing directly

--- bin/wasm-node/javascript/src/instance/raw-instance.ts
+++ bin/wasm-node/javascript/src/instance/raw-instance.ts
@@ -53,6 +53,7 @@ export async function startInstance(config: Config): Promise<SmoldotWasmInstance
     // Used to bind with the Wasi bindings. See the `bindings-wasi.js` file.
     const wasiConfig: WasiConfig = {
         envVars: [],
+        onProcExit: (retCode) => { throw new Error(`proc_exit called: ${retCode}`); }
     };
 
     // Start the Wasm virtual machine.
