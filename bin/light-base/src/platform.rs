eafe6367b34a51ff6ae3d1bce4cc20d156fec927
Pierre Krieger <pierre.krieger1708@gmail.com>
2022-09-16 12:12:28 +0200

    Fix build and small tweak

--- bin/light-base/src/platform.rs
+++ bin/light-base/src/platform.rs
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-use alloc::string::String;
+use alloc::{string::String, vec::Vec};
 use core::{ops, str, time::Duration};
 use futures::prelude::*;
 
