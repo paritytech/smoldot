b922230e43100005ff8e2578e334733ceee14faa
Pierre Krieger <pierre.krieger1708@gmail.com>
2022-10-10 15:04:19 +0200

    Spellcheck

--- bin/wasm-node/rust/src/init.rs
+++ bin/wasm-node/rust/src/init.rs
@@ -56,7 +56,7 @@ pub(crate) enum Chain {
         /// Receiver for JSON-RPC responses sent by the client. `None` if JSON-RPC requests are
         /// disabled on this chain.
         /// While this could in principle be a [`smoldot_light::JsonRpcResponses`], we wrap it
-        /// within a [`futures::Stream`] in order to guarantee that the waker that we register
+        /// within a [`futures::Stream`] in order to guarantee that the `waker` that we register
         /// doesn't get cleaned up.
         json_rpc_responses_rx: Option<stream::BoxStream<'static, String>>,
     },
