9e6a63ca86f5962bbe262e6ab4655852adae1307
Pierre Krieger <pierre.krieger1708@gmail.com>
2022-10-25 15:35:12 +0200

    Rustfmt

--- src/libp2p/connection.rs
+++ src/libp2p/connection.rs
@@ -79,7 +79,7 @@
 pub use noise::{NoiseKey, UnsignedNoiseKey};
 
 pub mod established;
-pub mod single_stream_handshake;
 pub mod multistream_select;
 pub mod noise;
+pub mod single_stream_handshake;
 pub mod yamux;
