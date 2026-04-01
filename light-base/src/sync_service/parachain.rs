7a51b4c3cc0a931784fcba55433c16fb0a0fc1be
Bastian Köcher <git@kchr.de>
2026-04-01 16:48:51 +0200

    Update light-base/src/sync_service/parachain.rs
    Co-authored-by: Alexandru Vasile <60601340+lexnv@users.noreply.github.com>

--- light-base/src/sync_service/parachain.rs
+++ light-base/src/sync_service/parachain.rs
@@ -536,10 +536,10 @@ pub(super) async fn start_parachain<TPlat: PlatformRef>(
                 let Some(sync) = &task.sync else {
                     unreachable!()
                 };
-                let fut = task
+                task
                     .network_service
-                    .set_local_best_block(*sync.best_block_hash(), sync.best_block_number());
-                fut.await;
+                    .set_local_best_block(*sync.best_block_hash(), sync.best_block_number())
+                    .await;
                 task.network_up_to_date_best = true;
             }
 
